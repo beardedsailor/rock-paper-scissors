c2be74c07ddc861e5f636f51c6b2e8fe5ffd1b94
Raghav Kapoor <42476325+beardedsailor@users.noreply.github.com>
2020-08-13 22:37:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const rock_div= document.getElementById("r");
 const paper_div= document.getElementById("p");
 const scissors_div= document.getElementById("s"); 
 
-
 function main(){
     rock_div.addEventListener('click',function(){
         game("r");
