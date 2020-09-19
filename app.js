e44a5adda892e4f8516238163710da4783c538fe
Raghav Kapoor <42476325+beardedsailor@users.noreply.github.com>
2020-09-19 23:07:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector('.scoreboard');
 const result_div = document.querySelector(".results > p");
+
 const rock_div= document.getElementById("r");
 const paper_div= document.getElementById("p");
 const scissors_div= document.getElementById("s"); 
